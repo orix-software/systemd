da34e92144efc47532a62ebdbe960378154077f0
jedeoric <plifplouf78@hotmail.com>
2021-12-20 22:28:33 +0100

    Add missing files

---
+++ src/commands/common/doscrolldowninframe.s
@@ -0,0 +1,10 @@
+.proc doscrolldowninframe
+    ldx     #$07
+    ldy     #24
+    BRK_KERNEL XSCROB
+    lda     #FRAME_VERTICAL_BAR_CHAR
+    sta     TWIL_INTERFACE_FIRST_LINE_TEXT
+    sta     TWIL_INTERFACE_FIRST_LINE_TEXT+39
+
+    rts
+.endproc
